077135e2d55d1403a6e85fb1202e1c262198b841
jotria <joana.tria@gcash.com>
2025-07-04 13:46:35 +0800

    Add main menu

--- Bank_System_2.0/src/main/java/org/example/Main.java
+++ Bank_System_2.0/src/main/java/org/example/Main.java
@@ -1,17 +1,74 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        int choice;
+        String accountType = "";
+        String accountNumber = "";
+        String bankAccountHolderName = "";
+        String initialDeposit = "";
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("===== Welcome to MyBank =====");
+
+        System.out.println("1. Create Account");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. Compute Interest");
+        System.out.println("5. Display Account");
+        System.out.println("6. Exit");
+
+        do {
+            System.out.print("\n Enter choice: ");
+            choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(choice){
+            case 1:
+                System.out.print("Enter Account Type: ");
+                accountType = scanner.nextLine();
+
+                System.out.print("Enter Account Number: ");
+                accountNumber = scanner.nextLine();
+
+                System.out.print("Enter Holder Name: ");
+                bankAccountHolderName = scanner.nextLine();
+
+                System.out.print("Initial deposit? (yes/no): ");
+                initialDeposit = scanner.nextLine();
+
+                if(initialDeposit.equals("yes")){
+                    System.out.print("Enter initial deposit amount: ");
+                    float initialDepositAmount = scanner.nextFloat();
+                    scanner.nextLine();
+
+                } else{
+
+                    break;
+                }
+
+                System.out.println("Account created successfully!");
+                break;
+
+            case 2:
+
+                break;
+            case 3:
+
+                break;
+            case 4:
+
+                break;
+            case 5:
+
+                break;
+            case 6:
+                System.out.println("-----Thank you for banking with us!-----");
+                break;
         }
+    } while(choice != 6);
+
     }
 }
